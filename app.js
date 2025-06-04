ac6904e93818585c1a5587e700413583f5d8a46a
Carlos Hernandez <krlosha30@gmail.com>
2025-06-04 15:45:45 -0600

    Moved the style from the html to the stylesheet file.

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
