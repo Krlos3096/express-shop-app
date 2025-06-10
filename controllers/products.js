aac90a6aec26c7248f1f560af83983d6ebc98033
Carlos Hernandez <krlosha30@gmail.com>
2025-06-10 16:26:39 -0600

    Added a JSON file to save the products.

--- controllers/products.js
+++ controllers/products.js
@@ -1,37 +1,38 @@
 import Product from "../models/product.js";
 
 const getAddProduct = (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
     formsCSS: true,
     productCSS: true,
-    activeAddProduct: true
+    activeAddProduct: true,
   });
 };
 
 const showProductAdded = (req, res, next) => {
-    const product = new Product(req.body.title);
-    product.save();
-  res.redirect('/');
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect("/");
 };
 
 const getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+    });
   });
-}
+};
 
 const ProductsController = {
   getAddProduct,
   showProductAdded,
-  getProducts
+  getProducts,
 };
 
 export default ProductsController;
--- models/product.js
+++ models/product.js
@@ -1,4 +1,8 @@
-const products = [];
+import fs from 'fs';
+import path from 'path';
+import __dirname from '../utils/path.js';
+
+const productsPath = path.join(__dirname, 'data', 'products.json');
 
 class Product {
     constructor(title) {
@@ -7,13 +11,30 @@ class Product {
     
     save() {
         // Logic to save the product, e.g., to a database
-        products.push(this);
+        fs.readFile(productsPath, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(productsPath, JSON.stringify(products), (err) => {
+                if (err) {
+                    console.error('Error writing to file', err);
+                }
+            });
+        });
         console.log(`Product saved: ${this.title}`);
     }
     
-    static fetchAll() {
+    static fetchAll(cb) {
         // Logic to fetch all products, e.g., from a database
-        return products;
+        return fs.readFile(productsPath, (err, fileContent) => {
+            if (err) {
+                console.error('Error reading file', err);
+                return cb([]);
+            }
+            return cb(JSON.parse(fileContent).products || []);
+        });
     }
 }
 
